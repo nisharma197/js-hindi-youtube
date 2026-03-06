a0acdcdd8088c717e4d64993b058c978b4e7cae9
nisharma197 <nishasharma4576@gmail.com>
2026-03-06 17:26:06 +0000

    datatypes summary

--- devcontainer/01_basics/03_conversionoperation.js
+++ devcontainer/01_basics/03_conversionoperation.js
@@ -1,27 +1,51 @@
-let score = 35
+// let score = 35
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isloggedIn = 1
+// let isloggedIn = 1
 
-let booleanIsLoggedIn = boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "nisha" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = string(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = string(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******* operations **********//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 =str1 + str2
+console.log(str3);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
---
+++ devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// primitive 
+
+// 7 types : string, number, boolean, null, undefined, symbol, Bigint
+
+const score = 100;
+const scoreValue = 100.1;
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail; 
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(id === anotherId); // false
+ const bigNumber = 9007199254740991n;
+
+// Reference types/ non-primitive
+
+// array, objects, functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "nisha",
+    age: 21,
+    isMarried: false,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof scoreValue);
+console.log(typeof myFunction);
