05b37ea256f8eb0f77d6e9029a1c72178856dbc2
nisharma197 <nishasharma4576@gmail.com>
2026-03-06 17:46:34 +0000

    datatypes summary

--- devcontainer/01_basics/datatypes-summary.js
+++ devcontainer/01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ const id = Symbol("123");
 const anotherId = Symbol("123");
 
 console.log(id === anotherId); // false
- const bigNumber = 9007199254740991n;
+//  const bigNumber = 9007199254740991n;
 
 // Reference types/ non-primitive
 
@@ -31,3 +31,26 @@ const myFunction = function(){
 
 console.log(typeof scoreValue);
 console.log(typeof myFunction);
+
+// ++++++++++++ memory +++++++++++++++++
+
+// stack (primitive), heap(non-primitive)
+
+let myname = "nisha"
+
+let anotherName = myname
+anothername = "nisha sharma"
+
+console.log(myname);
+console.log(anotherName);
+
+let userOne = {
+    name: "nisha",
+    age: 21,
+}
+
+let userTwo = userOne
+userTwo.name = "nisha sharma"
+
+console.log(userOne.name);
+console.log(userTwo.name);
